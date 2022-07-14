dc026ce91c25b14da7965b5863c7f54f058d6091
David Mijatovic <davidmijatovic@Davids-MacBook-Pro.local>
2022-07-14 10:48:32 +0200

    Setovao env varijable i komandu za login via BE

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://vivifyscrum-stage.com',
+    env: {
+      EXTERNAL_EMAIL: "jozomotika@gmail.com",
+      EXTERNAL_PASSWORD: "motikaJozo1"
+    },
+
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,17 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginViaBE', () => {
+    cy.request({
+        method: 'POST',
+        url: 'https://api.vivifyscrum-stage.com/api/v2/login',
+        body: {
+            email: Cypress.env('EXTERNAL_EMAIL'),
+            password: Cypress.env('EXTERNAL_PASSWORD')
+        }
+    }).its('body').then(response => {
+        window.localStorage.setItem('token', response.token);
+        window.localStorage.setItem('user', JSON.stringify(response.user));
+        window.localStorage.setItem('user_id', response.user.id);
+    })
+})
