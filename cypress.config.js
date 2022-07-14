c1999d940394bf76cb4024991fd72d9f88122d29
David Mijatovic <davidmijatovic@Davids-MacBook-Pro.local>
2022-07-14 11:04:06 +0200

    Dodao prazan red na kraju i dodao baseAPI

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://vivifyscrum-stage.com',
+    baseAPI: 'https://api.vivifyscrum-stage.com/api/v2',
     env: {
       EXTERNAL_EMAIL: "jozomotika@gmail.com",
       EXTERNAL_PASSWORD: "motikaJozo1"
